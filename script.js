86a7b718f50eef0e93d47dec2eac92fa0a1d12a5
jiwony933 <jiwony933@naver.com>
2022-07-11 16:20:13 +0900

    second-commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const home = document.querySelector("#home");
+const who = document.querySelector("#who");
+const portfolio = document.querySelector("#portfolio");
+const contact = document.querySelector("#contact");
 
+function handleHomeClick() {
+  window.scrollTo(0, 0);
+}
+
+home.addEventListener("click", handleHomeClick);
