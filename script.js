ffc633a6d9e83d16f234c56add4916bc5d73dc7e
Jiwon <jiwony933@naver.com>
2022-07-13 21:57:07 +0900

    scrollEvent

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const contactButton = document.querySelector("#menu-contact");
 
 function handleHomeClick() {
   window.scrollTo({ top: 0, behavior: "smooth" });
-  document.body.style.backgroundColor = "#76ba99";
-  document.body.style.transition = "0.5s";
 }
 function handleFavClick() {
   window.scrollTo({ top: 600, behavior: "smooth" });
-  document.body.style.backgroundColor = "lightblue";
-  document.body.style.transition = "0.5s";
 }
 function handlePortfolioClick() {
   window.scrollTo({ top: 1200, behavior: "smooth" });
-  document.body.style.backgroundColor = "beige";
-  document.body.style.transition = "0.5s";
 }
 function handleContactClick() {
   window.scrollTo({ top: 1800, behavior: "smooth" });
-  document.body.style.backgroundColor = "lightgrey";
-  document.body.style.transition = "0.5s";
 }
 
 homeButton.addEventListener("click", handleHomeClick);
 favButton.addEventListener("click", handleFavClick);
 portfolioButton.addEventListener("click", handlePortfolioClick);
 contactButton.addEventListener("click", handleContactClick);
+
+function handleScroll() {
+  let scrollY = this.scrollY;
+  console.log(scrollY);
+  if (scrollY >= 0 && scrollY < 400) {
+    document.body.style.backgroundColor = "#76ba99";
+    document.body.style.transition = "0.5s";
+  } else if (scrollY >= 400 && scrollY < 1000) {
+    document.body.style.backgroundColor = "lightblue";
+    document.body.style.transition = "0.5s";
+  } else if (scrollY >= 1000 && scrollY < 1600) {
+    document.body.style.backgroundColor = "beige";
+    document.body.style.transition = "0.5s";
+  } else if (scrollY >= 1600) {
+    document.body.style.backgroundColor = "lightgrey";
+    document.body.style.transition = "0.5s";
+  }
+}
+
+window.addEventListener("scroll", handleScroll);
